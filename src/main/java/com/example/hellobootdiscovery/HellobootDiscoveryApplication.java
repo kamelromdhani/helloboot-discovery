b5c1f4f658ef1e78a97e7d6f433d85dbdf40a795
Kamel Romdhani <Kamel.Romdhani@devoteam.be>
2019-12-23 11:54:57 +0100

    added annotations to main methode

--- src/main/java/com/example/hellobootdiscovery/HellobootDiscoveryApplication.java
+++ src/main/java/com/example/hellobootdiscovery/HellobootDiscoveryApplication.java
@@ -2,12 +2,16 @@ package com.example.hellobootdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RibbonClient(name = "simplecalcuator", configuration = SimpleCalculatorRibbonConfiguration.class)
 public class HellobootDiscoveryApplication {
 
 	public static void main(String[] args) {
